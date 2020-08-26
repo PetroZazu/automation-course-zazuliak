87373582967f9bf97c1705bcbaf648abfb40bf7d
Petro <petro.zazuliak@gmail.com>
2020-08-26 18:04:12 +0300

    test commit from new laptop

--- src/test/java/io/ctdev/AppTest.java
+++ src/test/java/io/ctdev/AppTest.java
@@ -10,8 +10,8 @@ public class AppTest {
     @Test
     public void shouldAnswerWithTrue() throws InterruptedException {
         WebDriver driver = new ChromeDriver();
-         driver.get("https://test.salesforce.com/");
-         Thread.sleep(10000);
-         driver.quit();
+        driver.get("https://test.salesforce.com/");
+        Thread.sleep(10000);
+        driver.quit();
     }
 }
