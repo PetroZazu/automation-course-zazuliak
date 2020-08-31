7191b3ca78d527dbc185fe7982621f8a2310cadc
Petro Zazuliak <petro.zazuliak@gmail.com>
2020-08-31 20:51:54 +0300

    HW task for lesson 3 - done

--- src/test/java/io/ctdev/lesson3/LocatorsForProfileEditPopUp.java
+++ src/test/java/io/ctdev/lesson3/LocatorsForProfileEditPopUp.java
@@ -2,7 +2,36 @@ package io.ctdev.lesson3;
 
 public class LocatorsForProfileEditPopUp {
     private String nameCss
-            = "div[class*='ownUserProfile_container'] [name=name]";
+            = "div[class*='ownUserProfile_container'] input[name=name]";
     private String nameXpath
-            = "//div[contains(@class, 'ownUserProfile_container')] //input[contains(@name, 'name')]";
+            = "//div[contains(@class, 'ownUserProfile_container')]//input[contains(@name, 'name')]";
+   // знаходжу основний контейнер (ownUserProfile_container) і в даному контейнері шукаю поле 'input'
+
+    private String nicknameCss
+            = "div[class*='ownUserProfile_container'] input[name='login'][class*='editUserProfile']";
+    private String nicknameXpath
+            = "//div[contains(@class, 'ownUserProfile_container')]//input[contains(@name, 'login')and contains(@class, 'edit')]";
+    // знаходжу основний контейнер (ownUserProfile_container) і в даному контейнері шукаю поле 'login' яке aka 'nickname'
+    // також я вирішив добавити атрибут клас в пошук для елементу текстового поля
+
+    private String PhoneNumberCss
+            = "div[class*='ownUserProfile_container'] input[name=phoneNumber]";
+    private String PhoneNumberXpath
+            = "//div[contains(@class, 'ownUserProfile_container')]//input[contains(@name, 'namephoneNumber')]";
+    // Same logic as for the first one
+
+    private String positionCss
+            = "div[class*='ownUserProfile_container'] input[name=position]";
+    private String positionXpath
+            = "//div[contains(@class, 'ownUserProfile_container')]//input[contains(@name, 'position')]";
+    // Same logic as for the first one
+
+    private String departmentCss
+            = "div[class*='ownUserProfile_container'] input[name=department]";
+    private String departmentXpath
+            = "//div[contains(@class, 'ownUserProfile_container')]//input[contains(@name, 'department')]";
+    // Same logic as for the first one
+
+
+
 }
